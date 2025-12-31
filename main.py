e7a4c1f9901f0a67c05801f7a8dec3e1a0dc2885
Zyberfenn <camtopp@gmail.com>
2025-12-30 21:39:14 -0500

    Player Class added & rotation implemented

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import *
 def main():
     pygame.init()
+    #instantiate clock
+    clock = pygame.time.Clock()
+    dt = 0
+    #instantiate player
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,8 +19,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-    
+        dt = clock.tick(60) / 1000
+        
+        
 if __name__ == "__main__":
     main()
